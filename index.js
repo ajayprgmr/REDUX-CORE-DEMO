2d2eed838fd10cd815def066c0a7eaff2ec07f6c
Ajay Kushwaha <ajay.kushwaha@horizonind.org>
2024-10-05 13:12:33 +0530

    redux-core-demo

---
+++ index.js
@@ -0,0 +1,40 @@
+const redux = require ( 'redux')
+const createStore = redux.createStore;
+
+
+const BUY_CAKE  = 'BUY_CAKE'
+
+function buyCake() {
+    return {
+        type:BUY_CAKE,
+        info:'First redux action'
+    }
+}
+
+
+const initialState =  {
+    numOfCakes:10
+}
+
+const reducer = (state=initialState, actions)=>{
+    switch(actions.type) {
+        case BUY_CAKE : return {
+            ...state,
+            numOfCakes:state.numOfCakes-1,
+        }
+
+        default : return state
+    }
+}
+
+
+const store = createStore(reducer)
+
+console.log('initial state',store.getState());
+
+const unsubscribe = store.subscribe(()=> console.log('updated state', store.getState()));
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+unsubscribe();
