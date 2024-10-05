f5f319760eb96fbcfa0056cce067ae84c921d9f9
Ajay Kushwaha <ajay.kushwaha@horizonind.org>
2024-10-06 00:40:55 +0530

    react-redux-basic example added

---
+++ react-redux-demo/src/components/cakeContainer.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { buyCake } from '../redux/cakes/cakeActions'
+import { connect } from 'react-redux'
+const cakeContainer = (props) => {
+    return (
+        <div>
+            <h2>No of Cakes - {props.numOfCakes}</h2>
+            <button
+                onClick={props.buyCake}
+            >
+                Buy Cake
+            </button>
+        </div>
+    )
+}
+
+
+const mapStateToProps = state => {
+    return {
+        numOfCakes: state.numOfCakes,
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake: () => dispatch(buyCake()),
+    }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(cakeContainer)
